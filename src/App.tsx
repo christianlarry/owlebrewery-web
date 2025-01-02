10157773f09c3e42364a1969d2c5141e165ba3e3
christianlarry <christianlarry03@gmail.com>
2025-01-02 22:20:28 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// IMPROT GLOBAL CSS
+import "./styles/global.css"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>OWLE COFFEE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
